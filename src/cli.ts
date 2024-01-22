7f98e4061cf79514a9c206dbdcb24aeb785129bd
Julian Gonzalez <joticajulian@gmail.com>
2024-01-22 01:09:07 +0100

    wip

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,16 @@
 import { program } from "commander";
 import inquirer, { Question } from "inquirer";
+import fs from "fs";
 import fse from "fs-extra";
 import path from "path";
+
+function updateFile(filename: string, patterns: string[], data: string[]) {
+  let filedata = fs.readFileSync(filename, "utf8");
+  patterns.forEach((pattern, i) => {
+    filedata = filedata.replace(new RegExp(pattern, "g"), data[i]);
+  });
+  fs.writeFileSync(filename, filedata);
+}
 
 async function main() {
   program.option("--name");
@@ -25,16 +34,29 @@ async function main() {
     } as unknown as Question);
   }
 
-  inquirer.prompt(questions).then((answers) => {
-    Object.keys(answers).forEach((key) => {
-      options[key] = answers[key];
-    });
+  await new Promise((resolve) => {
+    if (questions.length > 0) {
+      inquirer.prompt(questions).then((answers) => {
+        Object.keys(answers).forEach((key) => {
+          options[key] = answers[key];
+        });
+        resolve(true);
+      });
+    } else {
+      resolve(true);
+    }
   });
 
-  const folderName = options.name;
+  const folderName = (options.name as string).toLowerCase().replace(/ /g, "-");
 
   const sourceDir = path.join(__dirname, "templates/nft");
   fse.copySync(sourceDir, folderName);
+
+  updateFile(
+    path.join(folderName, "package.json"),
+    ["##_PROJECT_NAME_##"],
+    [folderName]
+  );
 }
 
 main()
