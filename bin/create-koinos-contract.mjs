6ec87f76723da2fd4f81f79983bfbc338e82181a
Julian Gonzalez <joticajulian@gmail.com>
2024-03-05 18:18:22 +0100

    fix: fix input arrow keys using spawn

--- bin/create-koinos-contract.mjs
+++ bin/create-koinos-contract.mjs
@@ -8,9 +8,9 @@ const args = [require.resolve("../lib/cli"), ...process.argv.slice(2)];
 
 const spawned = spawn("node", args, {
   env: Object.assign({}, process.env, { FORCE_COLOR: "1" }),
+  stdio: ["inherit"],
 });
 
-process.stdin.pipe(spawned.stdin);
 spawned.stdout.pipe(process.stdout);
 spawned.stderr.pipe(process.stderr);
 spawned.ref();
